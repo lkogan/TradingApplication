3f75da000dc586f7e019c56fcac34d768da439bc
Leonid Kogan <k_leo@hotmail.com>
2017-09-23 18:06:59 -0500

    Work in progress - commit.

---
+++ TradingApplication/StockPrice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingApplication
+{
+    public class StockPrice
+    {
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public DateTime QuoteDate { get; set; }
+    }
+
+    /* https://finance.yahoo.com/chart/MSFT  */
+
+    public interface IBacktester
+    {
+        /*
+         * Calculate the maximum profit you could have been made by buying and then selling 
+         * a single share over a given day range.
+         * Assume stockPrices are sorted by QuoteDate.
+         */
+        decimal CalculateMaxProfit(IEnumerable<StockPrice> stockPrices);
+    }
+
+
+    //TO DO: add date parameters
+
+    public class TradingCalcs : IBacktester
+    {
+        public decimal CalculateMaxProfit(IEnumerable<StockPrice> stockPrices)
+        {
+            List<decimal> lstLows = stockPrices.Select(x => x.Low).ToList();
+
+            List<decimal> lstHighs = stockPrices.Select(x => x.High).ToList();
+
+            decimal maxDifference = 0;
+
+            int i, j;
+            for (i = 0; i < lstLows.Count; i++)
+            {
+                for (j = 0; j < lstHighs.Count; j++)
+                {
+                    if (lstHighs[j] - lstLows[i] > maxDifference)
+                    {
+                        maxDifference = lstHighs[j] - lstLows[i];
+                    }
+                       
+                }
+            }
+            return maxDifference;
+
+        }
+    }
+
+}
---
+++ TradingApplication/Threading.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingApplication
+{
+    public class Query
+    {
+        public string Statement { get; set; }
+        public IDictionary<string, object> Params { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2
+    }
+
+    public interface IQueryExecutor
+    {
+        void Start();
+
+        void Stop();
+
+        void Schedule(Query query, Priority priority);
+    }
+
+
+    /*
+     * Implement IQueryExecutor interface. Class should execute SQL queries against relational database of your choice.
+     * 
+     * Requerements:
+     * - All queries executions should be consecutive (i.e. there should not be more than one query execution at a time).
+     * - All methods should be thread safe
+     */
+    public class Threading : IQueryExecutor
+    {
+        /*
+        * Start IQueryExecutor asynchronously.
+        */
+        public void Start()
+        {
+
+        }
+
+        /*
+         * Stop IQueryExecutor asynchronously.
+         */
+        public void Stop()
+        {
+
+        }
+
+        /*
+         * Schedule a new query for execution with corresponding priority.
+         * This method should not be blocked by query execution.
+         * Queries with higher priority should be executed before the ones with lower priority.
+         * If a query with a lower priority is being executed at the time, it may not be canceled (or re-scheduled).
+         */
+        public void Schedule(Query query, Priority priority)
+        {
+
+        }
+
+        public void RunQuery(Query query)
+        {
+
+        }
+    }
+}

e22e2af3e1a8faa2db6a718b3e94aee73a23c7c7
lkogan <k_leo@hotmail.com>
2018-05-21 09:40:17 -0500

    Add files via upload
    Assignment description

---
+++ Description/Task1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Query
+    {
+        public string Statement { get; set; }
+        public IDictionary<string, object> Params { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2
+    }
+
+    /*
+     * Implement IQueryExecutor interface. Class should execute SQL queries against relational database of your choice.
+     * 
+     * Requerements:
+     * - All queries executions should be consecutive (i.e. there should not be more than one query execution at a time).
+     * - All methods should be thread safe
+     */
+    public interface IQueryExecutor
+    {
+        /*
+         * Start IQueryExecutor asynchronously.
+         */
+        void Start();
+
+        /*
+         * Stop IQueryExecutor asynchronously.
+         */
+        void Stop();
+
+        /*
+         * Schedule a new query for execution with corresponding priority.
+         * This method should not be blocked by query execution.
+         * Queries with higher priority should be executed before the ones with lower priority.
+         * If a query with a lower priority is being executed at the time, it may not be canceled (or re-scheduled).
+         */
+        void Schedule(Query query, Priority priority);
+    }
+}
---
+++ Description/Task2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class StockPrice
+    {
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public DateTime QuoteDate { get; set; }
+    }
+
+
+    /*
+     * https://finance.yahoo.com/chart/MSFT
+     * 
+     * 
+     */
+    public interface IBacktester
+    {
+        /*
+         * Calculate the maximum profit you could have been made by buying and then selling a single share over a given day range.
+         * Assume stockPrices are sorted by QuoteDate.
+         */
+        decimal CalculateMaxProfit(IEnumerable<StockPrice> stockPrices);
+    }
+}
